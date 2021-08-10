c08c48c8cdc441362d9bde88b687024e87ccf081
unknown <aitsanjay6129@gmail.com>
2021-08-10 19:52:01 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const uid = "q13et4";
+
+var name = "sanjay";
+var email = "aitsanjaydfakj@gmail.com";
+var pass = "124qwe";
+var confirmpass = "124qwe";
+var coursecount = 0;
+var isLoggedfromgoogle = false;
+
+// name = prompt("enter your fullname");
+
+console.log(uid);
+console.log("Name of person is :", name);
+
+console.log(`
+with uid :  ${uid}
+    name is : ${name}
+    email is: ${email}
+`
+);
